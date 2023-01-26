50b7b974e1453957489ee6a2b28a329f9e529c2e
Yaroslav Ashchaulov <y.ashchaulov@gmail.com>
2023-01-26 22:28:34 +0200

    created function modal markup

---
+++ src/js/modal-window-markup.js
@@ -0,0 +1,38 @@
+export default function createMarkupModalWindow(data, filmGenres) {
+    const { title, poster_path, vote_average, vote_count, popularity, original_title, overview} = data;
+    const modalWindow = document.querySelector('.modal-film-info');
+    const markup = `<div class="poster-block">
+        <img class="poster" src="https://image.tmdb.org/t/p/w500/${poster_path}" />
+      </div>
+      <div class="film-information">
+        <h2 class="film-information-title">${title}</h2>
+        <table class="rating-table">
+          <tr>
+            <td class="table-subtitle">Vote / Votes</td>
+            <td class="table-inf"><span class="rating">${vote_average.toFixed(1)}</span> / ${vote_count}</td>
+          </tr>
+          <tr>
+            <td class="table-subtitle">Popularity</td>
+            <td class="table-inf">${popularity.toFixed(1)}</td>
+          </tr>
+          <tr>
+            <td class="table-subtitle">Original Title</td>
+            <td class="table-inf">${original_title}</td>
+          </tr>
+          <tr>
+            <td class="table-subtitle">Genre</td>
+            <td class="table-inf">${filmGenres}</td>
+          </tr>
+        </table>
+        <p class="about">About</p>
+        <p class="about-film">
+          ${overview}
+        </p>
+        <div class="buttons-add">
+          <button type="button" class="btn-add">add to Watched</button>
+          <button type="button" class="btn-add">add to queue</button>
+        </div>
+      </div>`
+    
+    modalWindow.innerHTML = markup;
+}
