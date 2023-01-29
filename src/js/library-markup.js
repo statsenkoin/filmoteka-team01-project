7d4b1d3173135c3c5bcd45c58db4f472b1eafaab
olena-semenenko <elen.kinakh@gmail.com>
2023-01-29 21:44:48 +0100

    Add render gallery in My Library
    Added render gallery.
    Added default page

--- src/js/library-markup.js
+++ src/js/library-markup.js
@@ -4,13 +4,45 @@ import { markupTrending } from './markups';
 const wachedBtn = document.querySelector('.js-watched');
 const queueBtn = document.querySelector('.js-queue');
 const gallery = document.querySelector('.home-gallery');
-console.log(wachedBtn);
-console.log(queueBtn);
-console.log(gallery);
+const defaultPage = document.querySelector('.default');
+console.dir(defaultPage.firstElementChild);
 
 wachedBtn.addEventListener('click', onWachedBtn);
+queueBtn.addEventListener('click', onQueueBtn);
 
 function onWachedBtn() {
+  clearGallery();
   const movies = watchedFilms.getLocalStorage();
-  markupTrending(movies, gallery);
+  if (!movies) {
+    showDefaultPage();
+    libraryError();
+  } else {
+    hiddenDefaultPage();
+    markupTrending(movies, gallery);
+  }
+}
+
+function onQueueBtn() {
+  clearGallery();
+  const movies = queueFilms.getLocalStorage();
+  if (!movies) {
+    showDefaultPage();
+    libraryError();
+  } else {
+    hiddenDefaultPage();
+    markupTrending(movies, gallery);
+  }
+}
+function libraryError() {
+  defaultPage.firstElementChild.textContent = 'There are no movies in your library yet..';
+  defaultPage.firstElementChild.style.color = '#ff001b';
+}
+function hiddenDefaultPage() {
+  defaultPage.classList.add('visually-hidden');
+}
+function showDefaultPage() {
+  defaultPage.classList.remove('visually-hidden');
+}
+function clearGallery() {
+  gallery.innerHTML = '';
 }
