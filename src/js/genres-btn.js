ff869cdb49cb53665ba1d0776ad1d117ab550384
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2023-02-01 22:02:45 +0200

    remov comments

--- src/js/genres-btn.js
+++ src/js/genres-btn.js
@@ -1,14 +1,10 @@
 import { dataGenres } from './test';
-// import { fetchMoivesByGenre } from './api-service';
-// import { markupTrending } from './markups';
 
 const dropdown = document.querySelector('.dropdown');
 const items = document.querySelector('.items');
-// const homeGallery = document.querySelector('.home-gallery');
 
-// =====================================
 export { items };
-// =====================================
+
 dropdown.addEventListener('click', onButtonClick);
 function btnGenres() {
   dataGenres.genres.unshift({ id: 'all', name: 'ALL' });
@@ -24,17 +20,9 @@ function btnGenres() {
   items.insertAdjacentHTML('beforeend', liArr);
 }
 
-// items.addEventListener('click', onGanreClick);
 btnGenres();
 
 function onButtonClick(evt) {
   evt.preventDefault();
   dropdown.classList.toggle('active');
 }
-
-// async function onGanreClick(evt) {
-//   evt.preventDefault();
-
-//   const arrMovies = await fetchMoivesByGenre(evt.target.id, 1);
-//   markupTrending(arrMovies.results, homeGallery);
-// }
